f5277c3843e4a30c64f0b6d35b2b13bc884866e5
Melanie Wang <melaniiewang@hotmail.com>
2023-08-10 19:08:27 -0400

    worked on the signupform correct imports and worked a bit on the loginform imports

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client'; 
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -12,6 +12,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, {error}] = useMutation (ADD_USER);
 
   useEffect(() => {
   if (error) {
